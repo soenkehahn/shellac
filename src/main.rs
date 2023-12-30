571125d8c81a6cb7d2eaf9a2a6be283fc88ead02
Sönke Hahn <soenkehahn@gmail.com>
2023-12-29 21:00:21 -0500

    Run alacritty with command from config

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,25 @@ use std::collections::BTreeMap;
 use std::error::Error;
 use std::fs;
 use std::io::ErrorKind;
+use std::os::unix::process::CommandExt;
+use std::process::Command;
 use swayipc::Connection;
 
 type R<A> = Result<A, Box<dyn Error>>;
 
 fn main() -> R<()> {
-    let workspace = find_active_workspace()?;
-    println!("active workspace: {workspace}");
-    let workspaces = read_config()?.workspaces;
-    println!("workspace config: {workspaces:?}");
+    let active_workspace = find_active_workspace()?;
+    println!("active workspace: {active_workspace}");
+    let config = read_config()?;
+    println!("config: {config:?}");
+    open_terminal(
+        "alacritty",
+        config
+            .workspaces
+            .get(&active_workspace)
+            .map(|x| x.as_ref())
+            .unwrap_or("zsh"),
+    )?;
     Ok(())
 }
 
@@ -39,4 +49,13 @@ fn read_config() -> R<Config> {
         Err(e) => return Err(e)?,
     };
     Ok(serde_yaml::from_reader(reader)?)
+}
+
+struct Never(Box<Never>);
+
+fn open_terminal(terminal: &str, command: &str) -> R<Never> {
+    let mut args: Vec<&str> = Vec::new();
+    args.push("--command");
+    args.append(&mut command.split_whitespace().collect());
+    Err(Command::new(terminal).args(args).exec())?
 }
